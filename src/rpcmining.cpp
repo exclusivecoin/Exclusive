1f34bd718bb1353371294e0a7cb2ef59ad115514
exclusivecoin <root@daemon.ip-192-99-17.net>
2015-05-05 00:05:30 +0000

    Fixed typo

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -68,7 +68,7 @@ Value getmininginfo(const Array& params, bool fHelp)
     weight.push_back(Pair("combined",  (uint64_t)nWeight));
     obj.push_back(Pair("stakeweight", weight));
 
-    if (pindexBest->nHeight < HFBLOCk) {
+    if (pindexBest->nHeight < HFBLOCK) {
       obj.push_back(Pair("stakeinterest",    (uint64_t)COIN_YEAR_REWARD));
     } else {
       obj.push_back(Pair("stakeinterest",    (uint64_t)HFCOIN_YEAR_REWARD));
